99972546aa86cab71bc86daae47701560b547482
maetecto <artur_paixao_de_matos@hotmail.com>
2021-11-07 19:32:15 +0000

    Problema com yarn start, so consigo testar o codigo desta forma

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,14 +1,20 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 const Home = () => {
     return (
+        <>
+        <NavBar />
         <div className="container">
             <div className="jumbotron">
-                <h1 className="display-4">DSVendastestes</h1>
+                <h1 className="display-4">DSVendas</h1>
                 <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
                 <hr />
                 <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
             </div>
         </div>
-
+        <Footer />
+        </>
     );
 }
 
